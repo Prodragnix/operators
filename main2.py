7148f1842e8499470c6eba3cdeab54a2a91dcc89
Prodragnix <connect.neilkumar@gmail.com>
2024-09-24 18:17:30 +0100

    Add files via upload

---
+++ main2.py
@@ -0,0 +1,13 @@
+x='Hello'
+y='Coding'
+print(x is y)
+print(id(x))
+print(id(y))
+list1=[1,2,3]
+list2=[1,2,3]
+print(list1 is not list2)
+print(id(list2))
+print(id(list1))
+a='I'
+b='I love chocolate'
+print(a in b)
